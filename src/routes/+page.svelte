fcc51f3376683d28766134d348ee216e062ce2c2
Javier Martínez Sabathier <javiermartinezsabathier@Javiers-MacBook-Air.local>
2023-10-08 02:07:01 -0300

    chore: refactor and clean code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,41 +10,27 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Photography</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-		<!-- <span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<Carousel >
+		<div style="width: 50%">
+			<img src={img1} alt="Armored Core VI. Developed by From Software" width="90%">
+		</div>
+		<div style="width: 50%">
+			<img src={img2} alt="Cyberpunk 2077. Developed by CD Projekt Red" width="80%">
+		</div>
+		<div style="width: 50%">
+			<img src={img3} alt="Cyberpunk 2077. Developed by CD Projekt Red" width="90%">
+		</div>
+		<div style="width: 50%">
+			<img src={img4} alt="Armored Core VI. Developed by From Software" width="75%">
+		</div>
+	</Carousel>
 
-		to your new<br />SvelteKit app -->
-		<Carousel >
-			<div style="width: 50%">
-				<img src={img1} alt="Armored Core VI. Developed by From Software" width="90%">
-			</div>
-			<div style="width: 50%">
-				<img src={img2} alt="Cyberpunk 2077. Developed by CD Projekt Red" width="80%">
-			</div>
-			<div style="width: 50%">
-				<img src={img3} alt="Cyberpunk 2077. Developed by CD Projekt Red" width="90%">
-			</div>
-			<div style="width: 50%">
-				<img src={img4} alt="Armored Core VI. Developed by From Software" width="75%">
-			</div>
-		</Carousel>
-
-		<Description/>
-
-	<!-- <h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter /> -->
+	<Description/>
 </section>
 
 <style>
@@ -59,21 +45,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
